f7fabdadc1b9035b8fd621d71690be80097d0b70
Daniele Vinci <vinci.dani@gmail.com>
2020-03-30 23:00:50 -0400

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text color="grey">
+        <span>SignOut</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <!-- <v-list-tile> -> <v-list-item> et al (anything -tile is now -item)
+      <v-list-tile-sub-title> -> <v-list-item-subtitle>-->
+      <v-list>
+        <v-list-item v-for="link in links" v-bind:key="link.text" router v-bind:to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
+    };
+  }
+};
+</script>
